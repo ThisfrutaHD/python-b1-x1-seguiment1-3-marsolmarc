e06692a6665b2fa6f3eaafe88aa8649af5ac870e
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-30 20:47:21 +0200

    add flake8 test

--- ejb1_x1_test.py
+++ ejb1_x1_test.py
@@ -1,6 +1,7 @@
 from ejb1_x1_main import *
 from util_package.text_manager import TEXT
 import pytest
+from flake8.api import legacy as flake8
 
 
 def test_largest_word():
@@ -65,3 +66,15 @@ def test_error_find_size_largest_sentence():
         ValueError
     ):
         find_size_largest_sentence(TEXT, "Texto No existe")
+
+
+def test_pep8_conformity():
+    style_guide = flake8.get_style_guide()
+    report = style_guide.check_files([
+        "ejb1_x1_main.py", 
+        "util_package/"
+    ])
+    
+    assert report.get_statistics("") == [], (
+        "Your code does not comply with flake8. Please review your code"
+    )
